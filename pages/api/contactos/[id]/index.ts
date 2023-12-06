e56852ac924672f5e8cfaa24c85144bd8dc8e779
Sergio Genes <genessergio@gmail.com>
2023-12-06 18:27:04 -0300

    Se agrega la ruta de api/contactos/[id]

--- pages/api/contactos/[id]/index.ts
+++ pages/api/contactos/[id]/index.ts
@@ -1,14 +1,23 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { Contacto, IContacto } from '../../../../models'
+import { db } from '../../../../db'
 
-type Data = {
-  name: string
-}
+type Data = IContacto | string
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
   const { id } = req.query
   console.log('id', id)
-  res.status(200).json({ name: 'incidente ID' })
+  await db.connectDB()
+  if (req.method === 'GET') {
+    console.log(req.method)
+    const contacto = await Contacto.findOne({
+      ID_de_contacto: id,
+    }).populate('Incidentes')
+    db.disconectDB()
+    if (contacto) res.status(200).send(contacto)
+    else res.status(404).send(`El usuario con ID ${id} no fue encontrado`)
+  }
 }
